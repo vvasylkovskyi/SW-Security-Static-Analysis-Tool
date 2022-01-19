cbdb65bbcbff15c4bb50c8624c2ca330469d1059
Francisco <franciscochenriques@tecnico.ulisboa.pt>
2022-01-19 13:17:51 +0000

    refactored basic visitors to OOP, so more complex visitors are easier to implement

--- src_visitor.py
+++ src_visitor.py
@@ -1,280 +1,66 @@
-"""
-Using the terms in the definition of the Python's AST module, the mandatory constructs are:
-    Literals (Constant),
-    Variables (Name),
-    Expressions (Expr, BinOp, Compare, Call, Attribute),
-    Statements (Assign),
-    Control flow (If and While)
-"""
+import visitors
 
-from collections import defaultdict
+class SrcVisitor(visitors.Visitor):
 
-#TODO indentation level, how to
+    def visit_Module(self, node):
+        self.indentation_level=0
+        return "\n".join(self.visit_body(node['body']))
 
-INDENTATION = "    "
+    def visit_Assign(self, node):
+        targets, value = super(SrcVisitor, self).visit_Assign(node)
+        return f"{self.indentation_level * SrcVisitor.INDENTATION}{','.join(targets)}={value}"
 
-def novisit(node, **kwargs):
-    raise NotImplementedError(f"no visitor implemented for {node['ast_type']}")
+    def visit_Call(self, node):
+        func, args = super(SrcVisitor, self).visit_Call(node)
+        return f"{func}({','.join(args)})"
 
-def visit_node(node, **kwargs):
-    # print(node);print()
-    return ast_type_visitors[node['ast_type']](node, **kwargs)
+    def visit_Expr(self, node):
+        value = super(SrcVisitor, self).visit_Expr(node)
+        return f"{self.indentation_level * SrcVisitor.INDENTATION}{value}"
 
-def visit_nodes(nodes, **kwargs):
-    return tuple(ast_type_visitors[e['ast_type']](e, **kwargs) for e in nodes)
+    def visit_BinOp(self, node):
+        (left, op, right) = super(SrcVisitor, self).visit_BinOp(node)
+        return f"{left}{op}{right}"
 
-def visit_Module(node, **kwargs):
-    """
-    :param node: {
-        'ast_type': 'Module',
-        'body': [...]
-    }
-    :return:
-    """
-    return visit_nodes(node['body'], indentation_level=0)
+    def visit_Compare(self, node):
+        (left, ops, comparators) = super(SrcVisitor, self).visit_Compare(node)
+        return f"{left}{','.join(ops)}{','.join(comparators)}"
 
-def visit_Assign(node, **kwargs):
-    """
-    :param node:
-    :return:
-    """
-    
-    targets = visit_nodes(node['targets'], **kwargs)
-    value = visit_node(node['value'], **kwargs)
-    # r = f"{','.join(targets)}={value}"
-    r = f"{kwargs['indentation_level']*INDENTATION}{','.join(targets)}={value}"
-    return r
+    def visit_While(self, node):
 
+        indentation = self.indentation_level * SrcVisitor.INDENTATION
+        self.indentation_level += 1
+        (test, body) = super(SrcVisitor, self).visit_While(node)
+        self.indentation_level -= 1
 
-def visit_Name(node, **kwargs):
-    """
-    :param node: {
-        'ast_type': 'Name',
-        'col_offset': 2,
-        'ctx': {'ast_type': 'Load'},
-        'id': 'b',
-        'lineno': 4
-    }
-    :return:
-    """
-    # alpha node['id']
-    return node['id']
+        representation = f"{indentation}while ({test}):\n"
+        representation += "\n".join(body)
+        return representation
+
+    def visit_If(self, node):
+
+        indentation = self.indentation_level * SrcVisitor.INDENTATION
+        self.indentation_level += 1
+        (test, body, orelse) = super(SrcVisitor, self).visit_If(node)
+        self.indentation_level -= 1
+
+        representation = f"{indentation}if({test}):\n"
+
+        representation += "\n".join(body)
+
+        if orelse:
+            representation += f"\n{indentation}else:\n"
+            representation += "\n".join(orelse)
+
+        return representation
+
+    def visit_Break(self, node):
+        return f"{self.indentation_level * SrcVisitor.INDENTATION}{super(SrcVisitor, self).visit_Break(node)}"
 
 
-def visit_Str(node, **kwargs):
-    """
-    :param node: {'ast_type': 'Str', 'col_offset': 2, 'lineno': 1, 's': ''}
-    :return:
-    """
-    return repr(node['s'])
+    def visit_Continue(self, node):
+        return f"{self.indentation_level * SrcVisitor.INDENTATION}{super(SrcVisitor, self).visit_Continue(node)}"
 
 
-def visit_Call(node, **kwargs):
-    """
-    :param node:
-        {'args': [],
-         'ast_type': 'Call',
-         'col_offset': 2,
-         'func': {'ast_type': 'Name',
-                  'col_offset': 2,
-                  'ctx': {'ast_type': 'Load'},
-                  'id': 'c',
-                  'lineno': 2},
-         'keywords': [],
-         'kwargs': None,
-         'lineno': 2,
-         'starargs': None
-        }
-    :return:
-    """
-    args = visit_nodes(node['args'], **kwargs)
-    func = visit_node(node['func'], **kwargs)
-    r = f"{func}({','.join(args)})"
-    return r
-
-
-def visit_Expr(node, **kwargs):
-    """
-    :param node:
-    :return:
-    """
-    
-    value = visit_node(node['value'], **kwargs)
-    return f"{kwargs['indentation_level']*INDENTATION}{value}"
-
-
-def visit_BinOp(node, **kwargs):
-    """
-    :param node:
-    :return:
-    """
-    left = visit_node(node['left'], **kwargs)
-    right = visit_node(node['right'], **kwargs)
-    op = visit_node(node['op'], **kwargs)
-    representation = f"{left}{op}{right}"
-    return representation
-
-
-def visit_Compare(node, **kwargs):
-    """
-    :param node:
-    :return:
-    """
-    left = visit_node(node['left'], **kwargs)
-    comparators = visit_nodes(node['comparators'], **kwargs)
-    ops = visit_nodes(node['ops'], **kwargs)
-    representation = f"{left}{','.join(ops)}{','.join(comparators)}"
-    return representation
-
-
-def visit_If(node, **kwargs):
-    """
-    :param node:
-    :return:
-    """
-
-    test = visit_node(node['test'], **kwargs)
-
-    indentation_level = kwargs['indentation_level']
-    indentation = indentation_level * INDENTATION
-
-    representation = f"{indentation}if({test}):\n"
-
-    body = visit_nodes(node['body'], indentation_level=indentation_level+1)
-    representation += "\n".join(body)
-
-    orelse = visit_nodes(node['orelse'], indentation_level=indentation_level+1)
-
-    if orelse:
-        representation += f"\n{indentation}else:\n"
-        representation += "\n".join(orelse)
-
-    return representation
-
-
-def visit_While(node, **kwargs):
-    """
-    :param node:
-    :return:
-    """
-    indentation_level = kwargs['indentation_level']
-    indentation = indentation_level * INDENTATION
-
-    test = visit_node(node['test'], **kwargs)
-    body = visit_nodes(node['body'], indentation_level=indentation_level+1)
-
-    representation = f"{indentation}while ({test}):\n"
-    representation += "\n".join(body)
-
-    return representation
-
-
-def visit_Break(node, **kwargs):
-    """
-    :param node:
-    :return:
-    """
-    return f"{kwargs['indentation_level'] * INDENTATION}break"
-
-
-def visit_Add(node, **kwargs):
-    """
-    :param node:
-    :return:
-    """
-    return '+'
-
-
-def visit_Num(node, **kwargs):
-    """
-    :param node:
-    :return:
-    """
-    return visit_node(node['n'], **kwargs)
-
-
-def visit_NotEq(node, **kwargs):
-    """
-    :param node:
-    :return:
-    """
-    return "!="
-
-
-def visit_Eq(node, **kwargs):
-    """
-    :param node:
-    :return:
-    """
-    return "=="
-
-
-def visit_int(node, **kwargs):
-    """
-    :param node:
-    :return:
-    """
-    key = "n_str"
-    return node[key]
-
-
-def visit_Gt(node, **kwargs):
-    """
-    :param node:
-    :return:
-    """
-    return ">"
-
-
-def visit_Lt(node, **kwargs):
-    """
-    :param node:
-    :return:
-    """
-    return "<"
-
-
-def visit_Load(node, **kwargs):
-    """
-    :param node:
-    :return:
-    """
-    raise NotImplementedError(f"visitor for ast_type Load not implemented")
-
-
-def visit_Store(node, **kwargs):
-    """
-    :param node:
-    :return:
-    """
-    # key = "k"
-    # return node[key]
-    raise NotImplementedError(f"visitor for ast_type Store not implemented")
-
-
-ast_type_visitors = defaultdict(lambda : novisit)
-
-#not really until really
-implemented_ast_type_visitors = {
-    'Store':visit_Store,
-    'Compare':visit_Compare,
-    'If':visit_If,
-    'NotEq':visit_NotEq,
-    'Eq':visit_Eq,
-    'Load':visit_Load,
-    'Expr':visit_Expr,
-    'Str':visit_Str,
-    'Break':visit_Break,
-    'Call':visit_Call,
-    'Module':visit_Module,
-    'BinOp':visit_BinOp,
-    'Name':visit_Name,
-    'While':visit_While,
-    'Lt':visit_Lt,
-    'Add':visit_Add,
-    'Num':visit_Num,
-    'Gt':visit_Gt,
-    'int':visit_int,
-    'Assign':visit_Assign,
-}
-ast_type_visitors.update(implemented_ast_type_visitors)
+if __name__ == '__main__':
+    visitors.Driver.drive(SrcVisitor)
---
+++ visitors.py
@@ -0,0 +1,338 @@
+"""
+"""
+
+
+class Visitor:
+
+    INDENTATION = "    "
+
+    def __init__(self, ast):
+        self.ast = ast
+        self.visit = list()
+
+    def visit_body(self, nodes):
+        visits = list()
+        for node in nodes:
+            ast_type = node['ast_type']
+            if ast_type == 'Expr':
+                visits.append(self.visit_Expr(node))
+            elif ast_type == 'Assign':
+                visits.append(self.visit_Assign(node))
+            elif ast_type == 'While':
+                visits.append(self.visit_While(node))
+            elif ast_type == 'If':
+                visits.append(self.visit_If(node))
+        return visits
+
+    def visit_operand(self, node):
+        return {
+            'Expr': self.visit_Expr,
+            'Call': self.visit_Call,
+            'Name': self.visit_Name,
+            'BinOp': self.visit_BinOp,
+            'Str': self.visit_Str,
+            'Num': self.visit_Num
+        }[node['ast_type']](node)
+
+
+    def visit_test(self, node):
+        return {
+            'Compare': self.visit_Compare,
+            'Expr': self.visit_Expr,
+            'Name': self.visit_Name,
+            'BinOp': self.visit_BinOp,
+            'Str': self.visit_Str,
+            'Num': self.visit_Num
+        }[node['ast_type']](node)
+
+
+    def visit_expression(self, node):
+        """
+        :param node:
+        :return:
+        """
+
+        ast_type = node['ast_type']
+        # print(ast_type)
+        if ast_type == 'Call':
+            expression = self.visit_Call(node)
+        elif ast_type == 'BinOp':
+            expression = self.visit_BinOp(node)
+        elif ast_type == 'Name':
+            expression = self.visit_Name(node)
+        elif ast_type == 'Str':
+            expression = self.visit_Str(node)
+        elif ast_type == 'Num':
+            expression = self.visit_Num(node)
+
+        return expression
+
+
+    def visit_ast(self):
+        return self.visit_Module(self.ast)
+
+
+    def visit_Module(self, node):
+        """
+        :param node: {
+            'ast_type': 'Module',
+            'body': [...]
+        }
+        :return:
+        """
+        return self.visit_body(node['body'])
+
+    def visit_Assign(self, node):
+        """
+        :param node:
+        :return:
+        """
+
+        targets = list(self.visit_Name(node) for node in node['targets'])
+
+        value = self.visit_expression(node['value'])
+
+        return targets, value
+
+
+    def visit_Call(self, node):
+        """
+        :param node:
+            {'args': [],
+             'ast_type': 'Call',
+             'col_offset': 2,
+             'func': {'ast_type': 'Name',
+                      'col_offset': 2,
+                      'ctx': {'ast_type': 'Load'},
+                      'id': 'c',
+                      'lineno': 2},
+             'keywords': [],
+             'kwargs': None,
+             'lineno': 2,
+             'starargs': None
+            }
+        :return:
+        """
+        func = self.visit_Name(node['func'])
+        
+        args = list(self.visit_expression(node) for node in node['args'])
+        
+        return func, args
+
+
+    def visit_Expr(self, node):
+        """
+        :param node:
+        :return:
+        """
+        value = self.visit_expression(node['value'])
+
+        return value
+
+
+    def visit_BinOp(self, node):
+        """
+        :param node:
+        :return:
+        """
+        left = self.visit_operand(node['left'])
+
+        op = {
+            'Add': self.visit_Add,
+            'Sub': self.visit_Sub
+        }[node['op']['ast_type']](node['op'])
+        
+        right = self.visit_operand(node['right'])
+
+        return (left, op, right)
+
+
+    def visit_Compare(self, node):
+        """
+        :param node:
+        :return:
+        """
+        left = self.visit_operand(node['left'])
+
+        comparators = list(self.visit_operand(node) for node in node['comparators'])
+
+        ops = list()
+        for node in node['ops']:
+            ast_type = node['ast_type']
+            if ast_type == 'NotEq':
+                ops.append(self.visit_NotEq(node))
+            elif ast_type == 'Eq':
+                ops.append(self.visit_Eq(node))
+            elif ast_type == 'Gt':
+                ops.append(self.visit_Gt(node))
+            elif ast_type == 'Lt':
+                ops.append(self.visit_Lt(node))
+
+        return (left, ops, comparators)
+
+
+    def visit_While(self, node):
+        """
+        :param node:
+        :return:
+        """
+        test = self.visit_test(node['test'])
+        body = self.visit_body(node['body'])
+        return test, body
+
+
+    def visit_If(self, node):
+        """
+        :param node:
+        :return:
+        """
+        test = self.visit_test(node['test'])
+        body = self.visit_body(node['body'])
+        orelse = self.visit_body(node['orelse'])
+        return test, body, orelse
+
+
+    def visit_Name(self, node):
+        """
+        :param node: {
+            'ast_type': 'Name',
+            'col_offset': 2,
+            'ctx': {'ast_type': 'Load'},
+            'id': 'b',
+            'lineno': 4
+        }
+        :return:
+        """
+        # alpha node['id']
+        return node['id']
+
+
+    def visit_Str(self, node):
+        """
+        :param node: {'ast_type': 'Str', 'col_offset': 2, 'lineno': 1, 's': ''}
+        :return:
+        """
+        return repr(node['s'])
+
+
+    def visit_Num(self, node):
+        """
+        :param node:
+        :return:
+        """
+        return self.visit_int(node['n'])
+
+
+    def visit_int(self, node):
+        """
+        :param node:
+        :return:
+        """
+        return node["n_str"]
+
+
+    def visit_Break(self, node):
+        """
+        :param node:
+        :return:
+        """
+        return "break"
+
+
+    def visit_Continue(self, node):
+        """
+        :param node:
+        :return:
+        """
+        return "continue"
+
+
+    def visit_Add(self, node):
+        """
+        :param node:
+        :return:
+        """
+        return '+'
+
+
+    def visit_Sub(self, node):
+        """
+        :param node:
+        :return:
+        """
+        return '-'
+
+
+    def visit_NotEq(self, node):
+        """
+        :param node:
+        :return:
+        """
+        return "!="
+
+
+    def visit_Eq(self, node):
+        """
+        :param node:
+        :return:
+        """
+        return "=="
+
+
+    def visit_Gt(self, node):
+        """
+        :param node:
+        :return:
+        """
+        return ">"
+
+
+    def visit_Lt(self, node):
+        """
+        :param node:
+        :return:
+        """
+        return "<"
+
+
+    def visit_Load(self, node):
+        """
+        :param node:
+        :return:
+        """
+        raise NotImplementedError(f"visitor for ast_type Load not implemented")
+
+
+    def visit_Store(self, node):
+        """
+        :param node:
+        :return:
+        """
+        # key = "k"
+        # return node[key]
+        raise NotImplementedError(f"visitor for ast_type Store not implemented")
+
+
+
+class Driver:
+    @staticmethod
+    def parser():
+        import argparse
+        parser = argparse.ArgumentParser(description=__doc__)
+        parser.add_argument("asts", type=str, nargs='+',
+                            help="path of a JSON file containing the program slice to analyse, represented in the form of an Abstract Syntax Tree")
+        return parser
+
+    @staticmethod
+    def drive(visitor):
+        from utilities import load_json
+        from pathlib import Path
+        args = Driver.parser().parse_args()
+        for ast in args.asts:
+            print(ast)
+            v = visitor(load_json(Path(ast)))
+            print(v.visit_ast())
+
+
+if __name__ == '__main__':
+    Driver.drive(Visitor)
